6631cb03e22f854d50b4ff0c3cb919f578b60944
Joshua Wang <joshuawang3110@gmail.com>
2019-02-10 01:05:10 -0500

    Change app name

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module BuzzHub
+module buzzhub_api
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
